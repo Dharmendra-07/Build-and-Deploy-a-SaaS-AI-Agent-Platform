2b47bfb5beba62146b86609c98f34090ee9fa3f9
justdk06 <justdk06@gmail.com>
2025-09-11 23:43:31 +0530

    Add layout and authentication views for sign-in and sign-up pages

---
+++ meetai/src/modules/auth/ui/views/sign-in-view.tsx
@@ -0,0 +1,12 @@
+'use client'
+import { Card } from "@/components/ui/card"
+
+export const SignInView =() =>{
+  console.log('Sign in view')
+
+  return (
+    <Card>
+      Sign in view
+    </Card>
+  )
+}
---
+++ meetai/src/modules/auth/ui/views/sign-up-view .tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import { Card } from "@/components/ui/card"
+
+export const SignUpView =() =>{
+  console.log('Sign up view')
+  
+  return (
+    <Card>
+      Sign in view
+    </Card>
+  )
+}
